503f240b37bad0174fb9f34376ca724b7c4c8131
YMS <yusufmertsunucu@hotmail.com>
2018-09-24 16:58:05 +0300

    Initial Commit

---
+++ AlarmProjects_UyarıOlusturma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlarmProjects_UyarıOlusturma
+//
+//  Created by YMS on 24.09.2018.
+//  Copyright © 2018 YMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
